30ddd54538f80e6dfe3c206712895b24f68bc54b
snak3ater <saeedyasir91@gmail.com>
2014-06-20 18:40:19 +0500

    Hammerhead LGE 4.4.4

--- hammerhead/BoardConfigPartial.mk
+++ hammerhead/BoardConfigPartial.mk
@@ -11,9 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-LOCAL_STEM := hammerhead/device-partial.mk
-
-$(call inherit-product-if-exists, vendor/broadcom/$(LOCAL_STEM))
-$(call inherit-product-if-exists, vendor/lge/$(LOCAL_STEM))
-$(call inherit-product-if-exists, vendor/qcom/$(LOCAL_STEM))
